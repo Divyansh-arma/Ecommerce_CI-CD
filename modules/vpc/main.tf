54544bb53745983cf88960950b839c7789c1c9cb
Divyansh-arma <sharmadivyansh003@gmail.com>
2025-08-27 20:18:13 +0530

    Updated the multi AZ by adding 2 subnets

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -12,7 +12,7 @@ module "vpc" {
   name = var.vpc_name
   cidr = var.vpc_cidr
 
-  azs             = [data.aws_availability_zones.available.names[0]]
+  azs             = slice(data.aws_availability_zones.available.names, 0, 2)
   private_subnets = var.private_subnets
   public_subnets  = var.public_subnets
 
--- variables.tf
+++ variables.tf
@@ -40,13 +40,13 @@ variable "vpc_cidr" {
 variable "public_subnets" {
   description = "List of public subnet CIDR blocks"
   type        = list(string)
-  default     = ["10.0.1.0/24"]
+  default     = ["10.0.1.0/24", "10.0.3.0/24"]
 }
 
 variable "private_subnets" {
   description = "List of private subnet CIDR blocks"
   type        = list(string)
-  default     = ["10.0.2.0/24"]
+  default     = ["10.0.2.0/24", "10.0.4.0/24"]
 }
 
 # EC2 Variables
